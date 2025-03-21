e15fc8b0bfc4455055e1f00147df9baa721f352b
Raiko Funakami <raiko.funakami@access-company.com>
2025-03-21 16:27:21 +0900

    [add] support Japanese name as alias

--- dummy_data/user.py
+++ dummy_data/user.py
@@ -2,23 +2,29 @@
 
 user_data = {
     "Takeshi": {
+        "user_name": "Takeshi",
         "viewer_role": "driver",
         "viewer_age": 40,
         "preferred_genres": ["documentary", "science", "travel"],
         "recent_watch_history": ["video001", "video002"]
     },
+    "たけし": "Takeshi",   # alias
     "Akiko": {
+        "user_name": "Akiko",
         "viewer_role": "driver",
         "viewer_age": 45,
         "preferred_genres": ["news", "sports", "action"],
         "recent_watch_history": ["video010", "video020"]
     },
+    "あきこ": "Akiko",    # alias
     "Kotaro": {
+        "user_name": "Kotaro",
         "viewer_role": "passenger",
         "viewer_age": 30,
         "preferred_genres": ["comedy", "drama", "anime"],
         "recent_watch_history": ["video100", "video200"]
-    }
+    },
+    "こたろう": "Kotaro"   # alias
 }
 
 if __name__ == "__main__":
